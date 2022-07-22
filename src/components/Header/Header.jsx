a8a06fdc8cbcde61ec02bfac8fe39b5fae88ddf9
Jafar Li-hammed <jafarli-hammed@Jafars-MacBook-Pro.local>
2022-07-22 22:36:54 +0100

    Home component done. It remains calories card

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
     <div className="header">
         <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1658441369/gym-fitness-center-video-logo-social-media-design-template-f9566537b0445b719d2c9ae5b4264d3c_screen_onlplf.jpg" alt="gym" className='logo' />
+
+        <ul className='menu'>
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
+        </ul>
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
-import Header from '../Header/Header';
-import './Home.css';
+import React from "react";
+import Header from "../Header/Header";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
-        <div className="left-h">
-          <Header />
+      <div className="left-h">
+        <Header />
+        {/* Best Ads */}
+        <div className="best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
         </div>
-        <div className="right-h">right</div>
-    </div>
-  )
-}
 
-export default Home
+        {/* Home text */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape</span>
+            <span> Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In this club we will help build you ideal body for male and female
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>experts coaches</span>
+          </div>
+          <div>
+            <span>+1080</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness progress</span>
+          </div>
+        </div>
+
+        {/* Hero buttons */}
+        <div className="buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart">
+          <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1658524427/2250px-Love_Heart_symbol.svg_s3ture.png" alt="heart-rate" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1658525145/fashionable-handsome-young-man-with-hairstyle-tattoo-his-shoulder-tying-his-laces-sneakers-street-sunset_338491-15298_yli5h4-removebg-preview_palizp.png" alt="guy" className="hero-image" />
+
+        <div className="calories">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
