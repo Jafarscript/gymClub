dab9e1ba0840ae69b296b0265e33da9fc728206f
Jafar Li-hammed <jafarli-hammed@Jafars-MacBook-Pro.local>
2022-08-04 18:59:04 +0100

    Join now button activated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './components/Home/Home.jsx'
+import Join from './components/Join/Join';
 import Plan from './components/Plans/Plan';
 import Program from './components/Program/Program';
 import Reason from './components/Reasons/Reason';
@@ -13,6 +14,7 @@ function App() {
       <Reason />
       <Plan />
       <Testimonial />
+      <Join />
     </div>
   );
 }
