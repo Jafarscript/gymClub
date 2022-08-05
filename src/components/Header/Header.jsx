f2d5596b68f682aa94256fb0803dd9fab4024e9e
Jafar Li-hammed <jafarli-hammed@Jafars-MacBook-Pro.local>
2022-08-05 22:32:42 +0100

    Mobile responsiveness not done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React , { useState} from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import './Header.css'
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="header">
         <img src="https://res.cloudinary.com/dvo8xhx6r/image/upload/v1658441369/gym-fitness-center-video-logo-social-media-design-template-f9566537b0445b719d2c9ae5b4264d3c_screen_onlplf.jpg" alt="gym" className='logo' />
-
-        <ul className='menu'>
-          <li>Home</li>
-          <li>Programs</li>
-          <li>Why us</li>
-          <li>Plans</li>
-          <li>Testimonials</li>
+        {(menuOpen === false  && mobile === true )?
+        (<div onClick={() => setMenuOpen(true)} className='ham'>
+          <GiHamburgerMenu />
+          </div>) : (
+            <ul className='menu'>
+          <li onClick={() => setMenuOpen(false)}>Home</li>
+          <li onClick={() => setMenuOpen(false)}>Programs</li>
+          <li onClick={() => setMenuOpen(false)}>Why us</li>
+          <li onClick={() => setMenuOpen(false)}>Plans</li>
+          <li onClick={() => setMenuOpen(false)}>Testimonials</li>
         </ul>
+          ) }
+        
     </div>
   )
 }
